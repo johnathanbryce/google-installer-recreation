03c7331c672f43dc6e52605076a365c74d929875
John Bryce <johnathanbryce@gmail.com>
2022-04-04 10:11:25 -0700

    pushing finalized code to github

--- src/components/Go.js
+++ src/components/Go.js
@@ -5,7 +5,7 @@ export default function Go() {
   return (
     <div>
       <ContentCard
-        moreInfo="For more information,  see: "
+        moreInfo="For more information, see: "
         language="Go"
         externalLink="https://cloud.google.com/go/docs/setup"
         installCommand="go get cloud.google.com/go/speech/apiv1"
